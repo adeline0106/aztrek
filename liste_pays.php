7d96c8ebb0841c80290fac87115583152faea939
adelinekammerer <adelinekammerer@172.17.48.158>
2018-07-30 19:37:51 +0200

    liste des pays / isotope

--- liste_pays.php
+++ liste_pays.php
@@ -8,7 +8,7 @@ $liste_sejours = getAllEntities("sejour");
 get_header("Liste des pays");
 ?>
 
-<section class="container">
+<section class="container isotope">
 
     <h2>Nos destinations</h2>
 
@@ -16,15 +16,17 @@ get_header("Liste des pays");
         <?php foreach ($liste_pays as $pays) : ?>
             <button class="button" data-filter=".pays<?php echo $pays["id"]; ?>">
                 <?php echo $pays["nom"]; ?></button>
-                <img src="uploads/<?php echo $pays["image"]; ?>" alt="<?php echo $pays["nom"]; ?>">
         <?php endforeach; ?>
     </div>
 
     <div class="grid">
         <?php foreach ($liste_sejours as $sejour) : ?>
-        <div class="element-item pays<?php echo $sejour["pays_id"]; ?>"><img src="uploads/<?php echo $sejour["image"]; ?>" alt="<?php echo $sejour["titre"]; ?>">
-            <h4><?php echo $sejour["titre"]; ?></h4>
+        
+        <div class="element-item pays<?php echo $sejour["pays_id"]; ?>"><a href="#"><img src="uploads/<?php echo $sejour["image"]; ?>" alt="<?php echo $sejour["titre"]; ?>">
+            <h4><?php echo $sejour["titre"]; ?></h4></a>
+            <p class="desc_courte"><?php echo $sejour["description_courte"]; ?></p>
         </div>
+        </a>
         <?php endforeach; ?>
     </div>
 
